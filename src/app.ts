98c6b1caa8e62f6b8a36f4cf2ab1bb48984b7f24
krutulkamil <krutulkamil@onet.pl>
2023-10-01 19:24:39 +0200

    feat: deserialize user middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import config from 'config';
 import { connect } from './utils/connect';
 import { log } from './utils/logger';
 import { router } from './routes';
+import { deserializeUser } from './middlewares/deserializeUser';
 
 const host = config.get<string>('host');
 const port = config.get<number>('port');
@@ -12,6 +13,8 @@ const protocol = config.get<boolean>('https') ? 'https' : 'http';
 const app = express();
 
 app.use(express.json());
+app.use(deserializeUser);
+
 app.use(router);
 
 app.listen(port, async () => {
---
+++ src/middlewares/deserializeUser.ts
@@ -0,0 +1,25 @@
+import type { Request, Response, NextFunction } from 'express';
+import { get } from 'lodash';
+
+import { verifyJwt } from '../utils/jwt';
+
+export const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken = get(req, 'headers.authorization', '').replace(
+    /^Bearer\s/,
+    ''
+  );
+
+  if (!accessToken) return next();
+
+  const decoded = verifyJwt(accessToken, 'accessTokenPublicKey');
+
+  if (decoded) {
+    res.locals.user = decoded;
+  }
+
+  return next();
+};
